4fa4520ad0f0a576fa8701c534e9ca1f0890d6bc
IamJon6275 <wel15010@byui.edu>
2023-02-08 09:47:09 -0700

    tournament

---
+++ tournaments/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Team firebirds = new Team("Firebirds");
+
+Player john = new Player("John Henderson", 12);
+//john.Display();
+Player tim = new Player("Tim Nunn", 1);
+
+firebirds.AddPlayer(john);
+firebirds.AddPlayer(tim);
+firebirds.DisplayRoster();
+
+Team blackSharks = new Team("Black Sharks");
+
+Player jon = new Player("Jonathan Wells", 1);
+Player jonas = new Player("Jonas Smith", 28);
+blackSharks.AddPlayer(jon);
+blackSharks.AddPlayer(jonas);
+
+Match match1 = new Match(firebirds, blackSharks);
+match1.DecideWin();
+
+blackSharks.DisplayRoster();
+firebirds.DisplayRoster();
